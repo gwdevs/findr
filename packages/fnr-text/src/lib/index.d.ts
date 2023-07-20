2397bd99c44c4ae3b6e14fc9c031b90d42913c75
Noah Harvey <noah.harvey247@gmail.com>
2023-07-20 15:01:59 -0400

    adds more todo comments

--- packages/fnr-text/src/lib/index.d.ts
+++ packages/fnr-text/src/lib/index.d.ts
@@ -6,11 +6,15 @@ export type Filter = (match: string) => string;
 
 //TODO: is this really config? Config generally implies a consumer's app will set these values ONCE and use everywhere.
 //for discoverability maybe we could rename this to SearchSettings?
+//TODO: this type has all Maybe keys. do we need them?
+//TODO: are all of these values being consumed in the core functionality?
 export interface FindrConfig {
   ctxLen?: number;
   filterCtxMatch?: Filter;
   filterCtxReplacement?: Filter;
   buildResultKey?: (index: number) => ResultKey;
+  //TODO: provide an explicitely defined type for XRegExp so users can more readly provide a value 
+  //TODO: typo, should be XRegeExp
   xregexp?: typeof XRegeExp;
   isRegex?: boolean;
   isCaseMatched?: boolean;
@@ -20,22 +24,25 @@ export interface FindrConfig {
 
 
 //TODO: make this well typed
-// 
+// A brief look at the source code reveals it's type could be:
 // {
-//  source: source,
-//  match: match,
-//  searchIndex,
-//  position: pos,
-//  groups: args,
-//  namedGroups,
+//  source: typeof source,
+//  match: typeof match,
+//  searchIndex: typeof searchIndex,
+//  position: typeof pos,
+//  groups: typeof args,
+//  namedGroups: typeof namedGroups,
 //  ...metadata,
 // }
 export type Metadata = { [key: string]: unknown };
 
+//TODO: extract the argument type (maybe create a more generic Callback type?)
+//TODO: return type has `string` blindness.
 export type ReplacementCallback = (params: {
   index: number;
   match: string;
   groups: Array<string>;
+  //TODO: use a more refined type like a Natural. Things like negative numbers should be impossible to use here.
   position: number;
   source: string;
   namedGroups: { [key: string]: unknown };
@@ -44,6 +51,9 @@ export type ReplacementCallback = (params: {
 export type ResultsAll = 'all';
 export type ResultKey = string | number;
 
+//TODO: avoid `string` type here. `string` implies ANY string. Does ANY string really work for these types?
+//TODO: often when a variable is either a `string | a` then we are actually defining a more refined type `b` with 2
+//constructors: `fromString : string -> b` and `fromA : a -> b`.
 export interface SearchAndReplace {
   source: string;
   target: string | RegExp;
@@ -54,6 +64,7 @@ export interface SearchAndReplace {
   config?: FindrConfig;
 }
 
+//TODO: rename the Context to something that is more meaningful. Namely, what is this the "context" of?
 export interface Context {
   before: string;
   after: string;
--- packages/fnr-text/src/lib/utils.ts
+++ packages/fnr-text/src/lib/utils.ts
@@ -1,10 +1,15 @@
+//TODO: this file seems uncessary for the following reasons:
+//  - it uses types (e.g RegExp) that were never imported
+//  - all of these functions have a single callsite
 export function escapeRegExp(string: string | RegExp) {
   const _string = string instanceof RegExp ? string.source : string;
   return _string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
+//TODO: the eval of a RegExp is to return the original source? Maybe we should rename this function?
 export function evalRegex(source: string | RegExp) {
   if (source instanceof RegExp) return source;
+  //TODO: runtime type checking. Typescript should already prevent the need for these checks.
   if (typeof source !== "string")
     throw new Error("Arg `source` shoud be of type string or RegExp");
   const results = source.match(/\/(.+)\/(?=(\w*$))/);
@@ -12,6 +17,7 @@ export function evalRegex(source: string | RegExp) {
   return { source: results[1], flags: results[2] };
 }
 
+//TODO: is this necessary? Will it handle languages defined in a non-ASCII language?
 export function isUpperCase(input: string) {
   return input.toUpperCase() === input && input.toLowerCase() !== input;
 }
