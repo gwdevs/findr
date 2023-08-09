a69019986cde5cfe87ce6157825cbfd41a3427d1
kintsoogi <kintsoogii@proton.me>
2023-08-09 11:59:34 +0530

    Added more readability comments

--- packages/fnr-react/src/lib/use-group-handlers/use-group-handlers.spec.tsx
+++ packages/fnr-react/src/lib/use-group-handlers/use-group-handlers.spec.tsx
@@ -3,6 +3,8 @@ import * as React from 'react';
 
 import useFnrCallbacks from './use-group-handlers';
 
+// TODO: Create more tests to cover the basic functionality of use-group-handlers
+
 describe('useFnrCallbacks', () => {
   it('should render successfully', () => {
     const { result } = renderHook(() => useFnrCallbacks());
