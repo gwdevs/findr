f760d51e1550a7abdb327ec9cce0d0a38e338b00
Abel Perez <abelper54@gmail.com>
2023-08-09 14:56:20 -0400

    proposal for new param and return types for findr

---
+++ packages/fnr-text/src/lib/findr.d.ts
@@ -0,0 +1,59 @@
+declare function findr<
+  TExtendedResultData extends Record<string, unknown>
+>(params: {
+  source: string;
+  target: string | RegExp;
+  replacement?: ReplacementPattern | string | ReplacementCallback;
+  replacementKeys?: Array<string | number> | 'all';
+  options?: {
+    isRegex?: boolean;
+    isCaseMatched?: boolean;
+    isWordMatched?: boolean;
+    isCasePreserved?: boolean;
+  };
+  hooks?: {
+    onSetResultKey?: (resultIndex: number) => number | string;
+    onSetResultData?: (result: SearchResultData) => TExtendedResultData;
+  };
+  config?: {
+    xregexp?: (pattern: string, flags?: string) => RegExp;
+    patterns?: {
+      wordLike?: string | RegExp;
+      upperCase?: string | RegExp;
+    };
+  };
+}): {
+  replaced: string;
+  results: (SearchResultData & TExtendedResultData)[];
+};
+
+interface MatchObject {
+  index: PositiveInteger;
+  match: FoundString;
+  groups: FoundSubstring[];
+  position: PositiveInteger;
+  source: string;
+  namedGroups: { [key: string]: unknown };
+}
+
+interface SearchResultData extends MatchObject {
+  index: PositiveInteger;
+  resultKey: string | number;
+  position: PositiveInteger;
+  match: FoundString;
+  context: {
+    before: SourceSubstringBeforeMatch;
+    after: SourceSubstringAfterMatch;
+  };
+  replacement: string;
+  groups: FoundSubstring[];
+  source: string;
+}
+
+type ReplacementPattern = string;
+type ReplacementCallback = (match: MatchObject) => string;
+type PositiveInteger = number;
+type FoundString = string;
+type FoundSubstring = string;
+type SourceSubstringBeforeMatch = string;
+type SourceSubstringAfterMatch = string;
