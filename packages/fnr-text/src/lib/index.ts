e641db67f2fe7f7dc921fe19db74a57bc0fc28eb
Abel Perez <abelper54@gmail.com>
2023-08-09 10:22:31 -0400

    added comments to findr-text

--- packages/fnr-text/src/lib/index.ts
+++ packages/fnr-text/src/lib/index.ts
@@ -1,6 +1,11 @@
 import { FindrParams, FindrResult, resultKey } from './index.d';
 import { escapeRegExp, evalRegex, isUpperCase } from './utils';
 
+/** 
+*  findr extends javascript's String.replace() by handling options like Preserve Case, 
+*  Match Word, Regex, and allowing consumers to extend it further.
+*  It formats it's response in a way that is easier to consume by a Find and Replace UI.
+*/
 export function findr({
   source,
   target,
@@ -19,25 +24,34 @@ export function findr({
     isCasePreserved = false,
   } = {},
 }: FindrParams) {
+  // START BUILDING SEARCH REGEXP
+
+  /** default flags to be used for regex pattern */
   const defaultFlags = ['g'];
-
   if (!isCaseMatched) defaultFlags.push('i');
-  const _isRegex = typeof isRegex === 'boolean' ? isRegex : isRegex === 'true';
 
+  /** is findr being used in regex mode */
+  const _isRegex = typeof isRegex === 'boolean' ? isRegex : isRegex === 'true';
   if (!_isRegex && target instanceof RegExp)
     console.warn('isRegex is set to false but target of type RegExp given.');
 
+  /** is user providing an instance of XRegExp */
   const isXre = xre instanceof Function;
 
+  /** regex engine (default or xregexp) */
   const regexer = isXre
     ? xre
     : function (source: string, flags = '') {
         return new RegExp(source, flags);
       };
 
-  const WordLike = isXre ? `p{Letter}\\p{Number}` : `\\w\\d`;
-  const UppercaseLetter = isXre ? `\\p{Uppercase_Letter}` : `[A-Z]`;
+  /** regex pattern for a wordlike character */
+  const WordLike = isXre ? `p{Letter}\\p{Number}` : `\\w\\d`; // needs increased support for multiple languages
 
+  /** regex pattern for uppercase character */
+  const UppercaseLetter = isXre ? `\\p{Uppercase_Letter}` : `[A-Z]`; // needs increased support for multiple languages
+
+  /** adds patterns needed to fit findr's config to a given RegExp */
   function prepareRegExp({
     regexp,
     isWordMatched,
@@ -51,20 +65,26 @@ export function findr({
       : regexer(`()(${source})`, flags);
   }
 
+  /** regex gotten from findr's target input */
   const rgxData = _isRegex
     ? evalRegex(target)
     : { source: escapeRegExp(target), flags: null };
 
+  /** merged default flags with inputted flags */
   const flags = rgxData.flags
     ? [...new Set([...rgxData.flags, ...defaultFlags])]
     : defaultFlags;
 
+  /** clean regex without findr's search configs */
   const finalRgx = regexer(rgxData.source, flags.join(''));
 
+  /** regex with findr's search config */
   const initialRgx = prepareRegExp({
     regexp: finalRgx,
     isWordMatched,
   });
+
+  //START FINDING AND REPLACING
 
   let searchIndex = 0;
   let replaceIndex = 0;
@@ -72,7 +92,11 @@ export function findr({
   const replaced =
     target !== ''
       ? source.replace(initialRgx, function (...args) {
+          // START BUILDING MATCH DATA
+
+          /** if the last argument of string.replace callback is an object it means the regexp contains groups */
           const containsGroup = typeof args[args.length - 1] === 'object';
+          /** get the groups if they exist and remove them from args */
           const namedGroups = containsGroup ? args.pop() : undefined;
           const source = args.pop();
           const tmpPos = args.pop();
@@ -81,6 +105,9 @@ export function findr({
           const pos = tmpPos + auxMatch.length;
           const match: string = args.shift();
 
+          // START BUILDING REPLACEMENT STRING
+
+          /** gets the replacement string from findr's replacement input */
           const replacementCB = function (): string {
             if (typeof replacement === 'function') {
               const rep = replacement({
@@ -101,8 +128,10 @@ export function findr({
             );
           };
 
+          /** replacement string from findr's replacement input */
           const r = replacementCB();
 
+          /** modifies replacement string according to findr's replacement config */
           const evaluateCase = (match: string, replaced: string) => {
             //TODO: Add callback to allow users to make their own case evaluation;
             if (!isCasePreserved) return replaced;
@@ -115,26 +144,38 @@ export function findr({
             return replaced;
           };
 
+          /** replacement string modified to match findr's replacement config */
           const replaced = evaluateCase(match, match.replace(finalRgx, r));
 
+          /** key for specific match index that needs to be replaced */
           const replacePointer: resultKey = buildResultKey
             ? buildResultKey(replaceIndex)
             : replaceIndex;
           replaceIndex++;
+
+          // REPLACE IF replacePointer IS INCLUDED IN replacementKeys given by user
+
           if (
             replacementKeys === 'all' ||
             replacementKeys.includes(replacePointer as string)
           ) {
+            /** if a replacementKey matches current result this result won't be included in the list of results */
             return auxMatch + replaced;
           }
 
+          // START BUILDING THE RESULT IF MATCH IS NOT REPLACED
+
+          /** substring before matched result */
           const ctxBefore = source.slice(pos - ctxLen, pos);
+          /** substring after matched result */
           const ctxAfter = source.slice(
             pos + match.length,
             pos + match.length + ctxLen
           );
 
+          /** all source text before matched result */
           const extCtxBefore = source.slice(0, pos);
+          /** all source text after matched result */
           const extCtxAfter = source.slice(pos + match.length, -1);
 
           const ctxMatch = filterCtxMatch ? filterCtxMatch(match) : match;
@@ -142,6 +183,7 @@ export function findr({
             ? filterCtxReplacement(replaced)
             : replaced;
 
+          /** creates a pointer to this result */
           const searchPointer = buildResultKey
             ? buildResultKey(searchIndex)
             : searchIndex;
