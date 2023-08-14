12f434182ac7f777f1e6c1644d7f34c291dfd5e9
Abel Perez <abelper54@gmail.com>
2023-08-14 13:37:16 -0400

    new signature

--- packages/fnr-text/src/lib/index.ts
+++ packages/fnr-text/src/lib/index.ts
@@ -1,174 +1,77 @@
-import { FindrParams, FindrResult, resultKey } from './index.d';
-import { escapeRegExp, evalRegex, isUpperCase } from './utils';
-
-export function findr({
+export function findr<TSearchResultExtension extends ResultRecord>({
   source,
-  target,
+  pattern,
   replacement = '',
   replacementKeys = [],
-  metadata,
-  config: {
-    filterCtxMatch = (match: string) => match,
-    filterCtxReplacement = (replacement: string) => replacement,
-    buildResultKey,
-    ctxLen = 0,
-    xregexp: xre,
+  options: {
     isRegex = false,
     isCaseMatched = true,
     isWordMatched = false,
     isCasePreserved = false,
   } = {},
-}: FindrParams) {
-  const defaultFlags = ['g'];
-
-  if (!isCaseMatched) defaultFlags.push('i');
-  const _isRegex = typeof isRegex === 'boolean' ? isRegex : isRegex === 'true';
-
-  if (!_isRegex && target instanceof RegExp)
-    console.warn('isRegex is set to false but target of type RegExp given.');
-
-  const isXre = xre instanceof Function;
-
-  const regexer = isXre
-    ? xre
-    : function (source: string, flags = '') {
-        return new RegExp(source, flags);
-      };
-
-  const WordLike = isXre ? `p{Letter}\\p{Number}` : `\\w\\d`;
-  const UppercaseLetter = isXre ? `\\p{Uppercase_Letter}` : `[A-Z]`;
-
-  function prepareRegExp({
-    regexp,
-    isWordMatched,
-  }: {
-    regexp: RegExp;
-    isWordMatched: boolean;
-  }) {
-    const { source, flags } = regexp;
-    return isWordMatched
-      ? regexer(`(^|[^${WordLike}])(${source})(?=[^${WordLike}]|$)`, flags)
-      : regexer(`()(${source})`, flags);
-  }
-
-  const rgxData = _isRegex
-    ? evalRegex(target)
-    : { source: escapeRegExp(target), flags: null };
-
-  const flags = rgxData.flags
-    ? [...new Set([...rgxData.flags, ...defaultFlags])]
-    : defaultFlags;
-
-  const finalRgx = regexer(rgxData.source, flags.join(''));
-
-  const initialRgx = prepareRegExp({
-    regexp: finalRgx,
-    isWordMatched,
-  });
-
-  let searchIndex = 0;
-  let replaceIndex = 0;
-  const results: FindrResult[] = [];
-  const replaced =
-    target !== ''
-      ? source.replace(initialRgx, function (...args) {
-          const containsGroup = typeof args[args.length - 1] === 'object';
-          const namedGroups = containsGroup ? args.pop() : undefined;
-          const source = args.pop();
-          const tmpPos = args.pop();
-          const tmpMatch = args.shift();
-          const auxMatch = args.shift();
-          const pos = tmpPos + auxMatch.length;
-          const match: string = args.shift();
-
-          const replacementCB = function (): string {
-            if (typeof replacement === 'function') {
-              const rep = replacement({
-                index: replaceIndex,
-                match,
-                groups: args,
-                position: pos,
-                source,
-                namedGroups,
-              });
-              return rep;
-            }
-            if (typeof replacement === 'string') {
-              return replacement;
-            }
-            throw new Error(
-              'Replacement param should be of type string or function.'
-            );
-          };
-
-          const r = replacementCB();
-
-          const evaluateCase = (match: string, replaced: string) => {
-            //TODO: Add callback to allow users to make their own case evaluation;
-            if (!isCasePreserved) return replaced;
-            if (isUpperCase(match)) {
-              return replaced.toUpperCase();
-            }
-            if (new RegExp(regexer(UppercaseLetter)).test(match[0])) {
-              return replaced[0].toUpperCase() + replaced.slice(1);
-            }
-            return replaced;
-          };
-
-          const replaced = evaluateCase(match, match.replace(finalRgx, r));
-
-          const replacePointer: resultKey = buildResultKey
-            ? buildResultKey(replaceIndex)
-            : replaceIndex;
-          replaceIndex++;
-          if (
-            replacementKeys === 'all' ||
-            replacementKeys.includes(replacePointer as string)
-          ) {
-            return auxMatch + replaced;
-          }
-
-          const ctxBefore = source.slice(pos - ctxLen, pos);
-          const ctxAfter = source.slice(
-            pos + match.length,
-            pos + match.length + ctxLen
-          );
-
-          const extCtxBefore = source.slice(0, pos);
-          const extCtxAfter = source.slice(pos + match.length, -1);
-
-          const ctxMatch = filterCtxMatch ? filterCtxMatch(match) : match;
-          const ctxReplacement = filterCtxReplacement
-            ? filterCtxReplacement(replaced)
-            : replaced;
-
-          const searchPointer = buildResultKey
-            ? buildResultKey(searchIndex)
-            : searchIndex;
-
-          //TODO: add result metadata as filterCtxReplacement arg
-          const result = {
-            match: ctxMatch,
-            replacement: ctxReplacement,
-            context: { before: ctxBefore, after: ctxAfter },
-            extContext: { before: extCtxBefore, after: extCtxAfter },
-            resultKey: searchPointer,
-            metadata: {
-              source: source,
-              match: match,
-              searchIndex,
-              position: pos,
-              groups: args,
-              namedGroups,
-              ...metadata,
-            },
-          };
-          results.push(result);
-          searchIndex++;
-          return tmpMatch;
-        })
-      : source;
+  hooks: { onSetResult } = {},
+  config,
+}: {
+  source: SourceText;
+  pattern: SearchPattern;
+  replacement?: ReplacementPattern | ReplacementCallback;
+  replacementKeys?: Array<Match['index']> | 'all';
+  options?: {
+    isRegex?: boolean;
+    isCaseMatched?: boolean;
+    isWordMatched?: boolean;
+    isCasePreserved?: boolean;
+  };
+  hooks?: {
+    onSetResult?: (result: FindrResult) => TSearchResultExtension;
+  };
+  config?: {
+    regexer?: <P extends SearchPattern | RegExp, F extends string>(
+      pattern: P,
+      flags: P extends RegExp ? undefined : F | undefined
+    ) => RegExp;
+    patterns?: {
+      wordLike?: SearchPattern;
+      uppercaseLetter?: SearchPattern;
+    };
+  };
+}): {
+  replaced: SourceTextWithReplacements;
+  results: (FindrResult & TSearchResultExtension)[];
+} {
+  //build results and replaced
   return { results, replaced };
 }
 
-export default findr
+type ResultRecord = Record<string, unknown>;
+
+interface Match {
+  index: PositiveInteger;
+  text: FoundString;
+  groups: FoundSubstring[];
+  position: PositiveInteger;
+  source: string;
+  namedGroups: { [groupName: string]: FoundSubstring };
+}
+
+interface FindrResult extends Match {
+  context: {
+    before: SourceSubstringBeforeMatch;
+    after: SourceSubstringAfterMatch;
+  };
+  replacement: string;
+}
+
+/**
+ * A searchable string. Findr suports searching English texts by default. Increase support by adding custom wordLike patterns to findr's config param.
+ */
+type SourceText = string;
+type SourceTextWithReplacements = SourceText;
+type SearchPattern = string | RegExp;
+type ReplacementPattern = string;
+type ReplacementCallback = (match: Match) => ReplacementPattern;
+type PositiveInteger = number;
+type FoundString = string;
+type FoundSubstring = string;
+type SourceSubstringBeforeMatch = string;
+type SourceSubstringAfterMatch = string;
